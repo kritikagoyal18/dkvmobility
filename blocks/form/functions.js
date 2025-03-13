bb34f9f572ad76f4b2c16679fa9d7bc0f3a6c7cf
Kritika Goyal <kritikagoyal@adobe.com>
2025-03-13 20:34:16 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -6,14 +6,18 @@
  * @return {string}
  */
 function getFullName(firstname, lastname) {
-  return `${firstname} ${lastname}`.trim();
+    const globals= arguments[arguments.length-1];    
+    firstname=(firstname==null)?"":firstname;
+    lastname=(lastname==null)?"":lastname;
+    console.log(globals);
+    return firstname.concat(' ').concat(lastname);
 }
 
 /**
  * Calculate the number of days between two dates.
  * @param {*} endDate
  * @param {*} startDate
- * @returns {number} returns the number of days between two dates
+ * @return {number} returns the number of days between two dates
  */
 function days(endDate, startDate) {
   const start = typeof startDate === 'string' ? new Date(startDate) : startDate;
@@ -28,5 +32,31 @@ function days(endDate, startDate) {
   return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
+/**
+ * Makes a REST call
+ * @name submitToRestEndpoint Submits the request to REST endpoint
+ * @return {string}
+ */
+function submitToRestEndpoint(){
+    console.log("hola!");
+    let result = "";
+    $.ajax({
+        url: '/content/dkvm-sign-endpoint',
+        type: 'POST',
+        //data: JSON.stringify({"actionNr": "8504"}),
+        success: function(response) {
+            alert("Thank you for submitting the form. We'll get back to you soon!");
+            console.log("success from post call");
+            result = "success";
+        },
+        error: function(xhr, status, error) {
+            alert("Thank you for submitting the form. We'll get back to you soon!");
+            console.error("Error: " + error);
+            result = "error";
+        }
+    });
+    return result;
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName, days };
+export { getFullName, days, submitToRestEndpoint };
